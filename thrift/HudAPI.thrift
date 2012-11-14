01dd51d9eb2197dd485ead7119294160e3006f20
Phillip Vassenkov <trainstroker@gmail.com>
2012-11-14 17:58:23 -0500

    updated hud api thrift file

--- thrift/HudAPI.thrift
+++ thrift/HudAPI.thrift
@@ -6,4 +6,14 @@ service HudFastPathService {
 	HudFastPathEvent executeFastPath(1: HudFastPathEvent event)
 }
 
+struct HudPIQLQuery {
+	1: string query
+}
 
+struct HudPIQLResponse {
+	1: string response
+}
+
+service HudPIQLService {
+	HudPIQLResponse executePIQLQuery(1: HudPIQLQuery)
+}
